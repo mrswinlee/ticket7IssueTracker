9d5b2db6efddd0ccf1fff0d9af520e5c91da92e7
Win Lee <mrswinlee@users.noreply.github.com>
2024-12-18 15:24:34 -0800

    Add files via upload

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { Button, Text, Callout, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
+
+const NewIssuePage = () => {
+  const router = useRouter();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({ resolver: zodResolver(createIssueSchema) });
+  const [error, setError] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setIsSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setIsSubmitting(false);
+      console.log(error);
+      setError("An unexpected error occurred.");
+    }
+  });
+
+  return (
+    <div className="max-w-xl space-y-3">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form onSubmit={onSubmit}>
+        {/* <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root> */}
+        <TextField.Root size="2" placeholder="Title" {...register("title")} />
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewIssuePage;
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Image from "next/image";
+
+const Home = () => {
+  return <div>Hello World</div>;
+};
+
+export default Home;
